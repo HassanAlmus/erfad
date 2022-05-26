6da007af77eb1449f58e2d4c57cc334f85748c18
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-05-27 00:39:14 +0300

    'l'

--- pages/index.tsx
+++ pages/index.tsx
@@ -653,7 +653,7 @@ const Home: NextPage = () => {
                   </div>
                   <ul role="list" className="flex justify-center space-x-5">
                       {person.twitterUrl!=='#'&&<li>
-                        <a target='_blank' href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+                        <a target='_blank' rel="noreferrer" href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">Twitter</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
@@ -661,7 +661,7 @@ const Home: NextPage = () => {
                         </a>
                       </li>}
                       {person.linkedinUrl!=='#'&&<li>
-                        <a target='_blank' href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                        <a target='_blank' rel="noreferrer" href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">LinkedIn</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path
@@ -673,7 +673,7 @@ const Home: NextPage = () => {
                         </a>
                       </li>}
                       {person.instagramUrl!=='#'&&<li>
-                        <a target='_blank' href={person.instagramUrl} className="text-gray-400 hover:text-gray-500">
+                        <a target='_blank' rel="noreferrer" href={person.instagramUrl} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">Instagram</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                           <path
@@ -685,7 +685,7 @@ const Home: NextPage = () => {
                         </a>
                       </li>}                   
                       <li>
-                        <a target='_blank' href={`mailto:${person.email}`} className="text-gray-400 hover:text-gray-500">
+                        <a target='_blank' rel="noreferrer" href={`mailto:${person.email}`} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">E-Mail</span>
                           <AtSymbolIcon className="w-5 h-5"/>
                         </a>
