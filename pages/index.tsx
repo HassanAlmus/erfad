5ec963e7d86823f4a97ba07925e0d24368b6c3ac
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-05-28 20:13:56 +0300

    kl

--- pages/index.tsx
+++ pages/index.tsx
@@ -402,8 +402,8 @@ const Home: NextPage = () => {
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
           <div className="text-center">
             <h1 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline">Erfad</span>{' '}
-              <span className="text-green">إرفاد</span>
+              <span className="inline">Erfad</span>{' '}
+              <span className="inline text-green">إرفاد</span>
             </h1>
             <div className='flex justify-between flex-wrap-reverse gap-10'>
             <p className="w-[400px] text-left mt-3 max-w-md mx-auto text-2xl text-gray-500 md:mt-5 md:max-w-3xl">
