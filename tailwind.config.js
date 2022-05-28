0904e4ac5e41eb0d6caef58eb1235c2cb72e1a62
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-05-28 14:58:35 +0300

    j

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./pages/index.tsx'],
+  content: ['./pages/index.tsx', './pages/members.tsx'],
   theme: {
     extend: {
       fontFamily: {
