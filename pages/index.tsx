2bd073e94962e8a67fc6ca83f1e19a26a2449af9
Hassan Al Mosapeeh <hassanalmosapeeh@gmail.com>
2022-06-19 19:45:06 +0300

    ll

--- pages/index.tsx
+++ pages/index.tsx
@@ -275,8 +275,42 @@ export const Footer =()=> (
 )
 
 const Home: NextPage = () => {
+  const [show, setShow] = useState(true)
   return (
     <>
+      {show&&<div className="bg-blue">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="w-0 flex-1 flex items-center">
+            <span className="flex p-2 rounded-lg bg-darkblue">
+              <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </span>
+            <p className="ml-3 font-medium text-white truncate">
+              <span className="md:hidden">Applications Open! | التقديم مفتوح!</span> 
+              <span className="hidden md:inline">Membership Applications Are Open! | التقديم على العضوية مفتوح</span>
+            </p>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="https://forms.gle/fv77r9ALQv22axgA6"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-darkblue bg-white hover:bg-indigo-50"
+            >
+             Apply | قدّم
+            </a>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex p-2 rounded-md hover:bg-darkblue focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              onClick={()=>setShow(false)}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>}
     <div className="relative bg-gray-50 overflow-hidden">
       <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
         <div className="relative h-full max-w-7xl mx-auto">
