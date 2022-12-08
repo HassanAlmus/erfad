745a165b5d0f948d8c850b387aee33dd8d6156ff
حسن المسبح <hsnalmsbh@Hassans-MacBook-Air.local>
2022-12-08 13:45:29 +0300

    jk

--- pages/index.tsx
+++ pages/index.tsx
@@ -276,38 +276,7 @@ const Home: NextPage = () => {
   const [show, setShow] = useState(true)
   return (
     <>
-    <div className="bg-green">
-      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between flex-wrap">
-          <div className="w-0 flex-1 flex items-center">
-            <span className="flex p-2 rounded-lg bg-darkgreen">
-              <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </span>
-            <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">برنامج حوار أخضر</span> 
-              <span className="hidden md:inline">برنامج حوار أخضر</span> 
-            </p>
-          </div>
-          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-            <a
-              href="https://forms.gle/59XrybbmZr7L2Vqa6"
-              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-darkblue bg-white hover:bg-indigo-50"
-            >
-            سجّل الآن!
-            </a>
-          </div>
-          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3 opacity-0">
-            <button
-              type="button"
-              className="-mr-1 flex p-2 rounded-md hover:bg-darkblue focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-            >
-              <span className="sr-only">Dismiss</span>
-              <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+ 
     <div className="relative bg-gray-50 overflow-hidden">
       <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
         <div className="relative h-full max-w-7xl mx-auto">
@@ -608,7 +577,7 @@ const Home: NextPage = () => {
               <div className="relative">
                 <dt>
                   <CheckIcon className="absolute h-6 w-6 text-green" aria-hidden="true" />
-                  <p className="ml-9 text-2xl leading-6 font-medium text-gray-900">Honesty | الإمانة</p>
+                  <p className="ml-9 text-2xl leading-6 font-medium text-gray-900">Honesty | الأمانة</p>
                 </dt>
               </div>
               <div className="relative">
