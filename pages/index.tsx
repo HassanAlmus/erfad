3118d058722102e91e6ced1ad7554a55a1a275a1
حسن المسبح <hsnalmsbh@Hassans-MacBook-Air.local>
2023-01-04 14:47:28 +0300

    j

--- pages/index.tsx
+++ pages/index.tsx
@@ -517,7 +517,7 @@ const Home: NextPage = () => {
   const [show, setShow] = useState(true)
   return (
     <>
- {show&&<div className="bg-blue">
+{/*  {show&&<div className="bg-blue">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
@@ -535,6 +535,39 @@ const Home: NextPage = () => {
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-darkblue bg-white hover:bg-indigo-50"
             >
              Contact | تواصل
+            </a>
+          </div>
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className="-mr-1 flex p-2 rounded-md hover:bg-darkblue focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              onClick={()=>setShow(false)}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>}  */}
+     {show&&<div className="bg-blue">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="w-0 flex-1 flex items-center">
+            <span className="flex p-2 rounded-lg bg-darkblue">
+              <QuestionMarkCircleIcon className="h-6 w-6 text-white" aria-hidden="true" />
+            </span>
+            <p className="ml-3 font-medium text-white truncate">
+              <span className="md:hidden">Want to join us? | تريد الانضمام؟</span> 
+              <span className="hidden md:inline">Want to join us? | تريد الانضمام؟</span> 
+            </p>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <a
+              href="https://forms.gle/eTLxERjXgML8bBwz6"
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-darkblue bg-white hover:bg-indigo-50"
+            >
+             Apply | التقديم
             </a>
           </div>
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
